c9d5eeaed3fe1437e008994e8792b6732373d0b5
Muhammad Faizan <mohammadfaizan410@gmail.com>
2023-05-11 01:30:40 +0300

    trying

--- getAllBroadcasts.php
+++ getAllBroadcasts.php
@@ -5,7 +5,7 @@ require_once("./config-messages.php");
 
 if(isset($_POST['email'])){
     $user_email = $_POST['email'];
-    $sql = "SELECT * FROM broadcast WHERE to_email = :to_email order by id desc";
+    $sql = "SELECT * FROM broadcast WHERE to_email = 'fai@gmail.com' order by id desc";
     $stmt = $db->prepare($sql);
     $stmt->bindParam(':to_email', $user_email);
     $stmt->execute();
