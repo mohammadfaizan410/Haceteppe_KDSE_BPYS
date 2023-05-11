d7a3ce8e2536b92f49caf14bd484b61890ef4ca8
Muhammad Faizan <mohammadfaizan410@gmail.com>
2023-05-11 22:01:39 +0300

    configs-live

--- config-students.php
+++ config-students.php
@@ -1,8 +1,8 @@
 
 <?php
-$db_user = "root";
-$db_pass = "";
+$db_user = "admin";
+$db_pass = "12345678";
 $db_name = "kdse_db";
-$db_host = "localhost";
-$db = new PDO('mysql:host=' . $db_host . ';dbname=' . $db_name . ';charset-utf8', $db_user, $db_pass);
+$db_host = "ebyrys.cpcbxswdfvjg.eu-central-1.rds.amazonaws.com";
+$db = new PDO('mysql:host=' . $db_host .';dbname=' . $db_name . ';charset-utf8', $db_user, $db_pass);
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
