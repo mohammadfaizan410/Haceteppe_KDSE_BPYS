03a568a92e7fb71256f7cae5c70fb7387ef1394c
ozanergun98 <99257197+ozanergun98@users.noreply.github.com>
2023-05-12 04:13:26 +0300

    Update kateterForm1.php

--- formlar/kateterForm1.php
+++ formlar/kateterForm1.php
@@ -75,15 +75,15 @@ if (isset($_GET['logout'])) {
 
                         <div class="checkboxes w-25">
                             <div class="form-check">
-                                <input class="form-check-input" type="radio" name="beslenmeiledurumuradio" id="beslenmeileradio" value="option1">
-                                <label class="form-check-label" for="beslenmeileumuradio">
+                                <input class="form-check-input" type="radio" name="venöz_kateter" id="venöz_kateter" value="Periferik venöz kateter ">
+                                <label class="form-check-label" for="venöz_kateter">
                                     <span class="checkbox-header">Periferik venöz kateter </span>
                                 </label>
                             </div>
                         </div>
-                        <input type="text" class="form-control w-25">
-                        <input type="text" class="form-control w-25">
-                        <input type="text" class="form-control w-25">
+                        <input type="text" class="form-control w-25" name="PYeri" id="PYeri">
+                        <input type="text" class="form-control w-25" name="PSayısı" id="PSayısı">
+                        <input type="text" class="form-control w-25" name="PTakılmaTarihi" id="PTakılmaTarihi">
                     </div>
                 </div>
                 <div class="d-flex justify-content-between">
@@ -91,15 +91,15 @@ if (isset($_GET['logout'])) {
 
                         <div class="checkboxes w-25">
                             <div class="form-check">
-                                <input class="form-check-input" type="radio" name="beslenmeiledurumuradio" id="beslenmeileradio" value="option1">
-                                <label class="form-check-label" for="beslenmeileumuradio">
+                                <input class="form-check-input" type="radio" name="venöz_kateter" id="venöz_kateter" value="Santral venöz kateter">
+                                <label class="form-check-label" for="venöz_kateter">
                                     <span class="checkbox-header">Santral venöz kateter </span>
                                 </label>
                             </div>
                         </div>
-                        <input type="text" class="form-control w-25">
-                        <input type="text" class="form-control w-25">
-                        <input type="text" class="form-control w-25">
+                        <input type="text" class="form-control w-25" name="SYeri" id="SYeri">
+                        <input type="text" class="form-control w-25" name="SSayısı" id="SSayısı">
+                        <input type="text" class="form-control w-25" name="STakılmaTarihi" id="STakılmaTarihi">
                     </div>
                 </div>
                 <div class="d-flex justify-content-between">
@@ -107,15 +107,15 @@ if (isset($_GET['logout'])) {
 
                         <div class="checkboxes w-25">
                             <div class="form-check ">
-                                <input class="form-check-input" type="radio" name="beslenmeiledurumuradio" id="beslenmeileradio" value="option1">
-                                <label class="form-check-label" for="beslenmeileumuradio">
+                                <input class="form-check-input" type="radio" name="venöz_kateter" id="venöz_kateter" value="option1">
+                                <label class="form-check-label" for="venöz_kateter">
                                     <span class="checkbox-header">Dren </span>
                                 </label>
                             </div>
                         </div>
-                        <input type="text" class="form-control w-25">
-                        <input type="text" class="form-control w-25">
-                        <input type="text" class="form-control w-25">
+                        <input type="text" class="form-control w-25" name="DYeri" id="DYeri">
+                        <input type="text" class="form-control w-25" name="DSayısı" id="DSayısı">
+                        <input type="text" class="form-control w-25" name="DTakılmaTarihi" id="DTakılmaTarihi">
                     </div>
                 </div>
                 <div class="d-flex justify-content-between">
@@ -123,58 +123,18 @@ if (isset($_GET['logout'])) {
 
                         <div class="checkboxes w-25">
                             <div class="form-check">
-                                <input class="form-check-input" type="radio" name="beslenmeiledurumuradio" id="beslenmeileradio" value="option1">
-                                <label class="form-check-label" for="beslenmeileumuradio">
+                                <input class="form-check-input" type="radio" name="venöz_kateter" id="venöz_kateter" value="Diğer">
+                                <label class="form-check-label" for="venöz_kateter">
                                     <span class="checkbox-header">Diğer</span>
                                 </label>
                             </div>
                         </div>
-                        <input type="text" class="form-control w-25">
-                        <input type="text" class="form-control w-25">
-                        <input type="text" class="form-control w-25">
+                        <input type="text" class="form-control w-25" name="DiğerYeri" id="DiğerYeri">
+                        <input type="text" class="form-control w-25" name="DiğerSayısı" id="DiğerSayısı">
+                        <input type="text" class="form-control w-25" name="DiğerTakılmaTarihi" id="DiğerTakılmaTarihi">
                     </div>
                 </div>
             </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
             <script>
                 $(function() {
@@ -193,28 +153,46 @@ if (isset($_GET['logout'])) {
                         var valid = this.form.checkValidity();
 
                         if (valid) {
-                            var id = "<?php
+                            var id = <?php
 
-                            $userid = $_SESSION['userlogin']['id'];
-                            echo $userid;
-                                ?>"
-                            var name = $('#name').val();
-                            var surname = $('#surname').val();
-                            var age = $('#age').val();
-                            var not = $('#not').val();
+                                        $userid = $_SESSION['userlogin']['id'];
+                                        echo $userid
+                                        ?>;
+                            let venöz_kateter = $("input[name='venöz_kateter']:checked").val();
+                            let PYeri = $("input[type='radio'][name='PYeri']").val();
+                            let PSayısı = $("input[type='radio'][name='PSayısı']").val();
+                            let PTakılmaTarihi = $("input[type='radio'][name='PTakılmaTarihi']").val();
+                            let SYeri = $("input[type='radio'][name='SYeri']").val();
+                            let SSayısı = $("input[type='radio'][name='SSayısı']").val();
+                            let STakılmaTarihi = $("input[type='radio'][name='STakılmaTarihi']").val();
+                            let DYeri = $("input[type='radio'][name='DYeri']").val();
+                            let DSayısı = $("input[type='radio'][name='DSayısı']").val();
+                            let DTakılmaTarihi = $("input[type='radio'][name='DTakılmaTarihi']").val();
+                            let DigerYeri = $("input[type='radio'][name='DigerYeri']").val();
+                            let DigerSayısı = $("input[type='radio'][name='DigerSayısı']").val();
+                            let DigerTakılmaTarihi = $("input[type='radio'][name='DigerTakılmaTarihi']").val();
+
 
 
                             e.preventDefault()
 
                             $.ajax({
                                 type: 'POST',
-                                url: '<?php echo $base_url; ?>/student-patient.php',
+                                url: 'student-patient.php',
                                 data: {
-                                    id: id,
-                                    name: name,
-                                    surname: surname,
-                                    age: age,
-                                    not: not
+                                    venöz_kateter: venöz_kateter,
+                                    PYeri: PYeri,
+                                    PSayısı: PSayısı,
+                                    PTakılmaTarihi: PTakılmaTarihi,
+                                    SYeri: SYeri,
+                                    SSayısı: SSayısı,
+                                    STakılmaTarihi: STakılmaTarihi,
+                                    DYeri: DYeri,
+                                    DSayısı: DSayısı,
+                                    DTakılmaTarihi: DTakılmaTarihi,
+                                    DigerYeri: DigerYeri,
+                                    DigerSayısı: DigerSayısı,
+                                    DigerTakılmaTarihi: DigerTakılmaTarihi
 
                                 },
                                 success: function(data) {
@@ -235,8 +213,7 @@ if (isset($_GET['logout'])) {
                         }
                     })
 
-                }); </script> < !--JavaScript Libraries-- > <
-                script src = "https://code.jquery.com/jquery-3.4.1.min.js" >
+                });
             </script>
             <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
             <script src="lib/chart/chart.min.js"></script>
