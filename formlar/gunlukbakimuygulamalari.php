0d8e07c41689ae36afdc34774279351ccaafe93d
Semih Demirler <semihdemirler@gmail.com>
2023-05-12 03:27:23 +0300

    Update gunlukbakimuygulamalari.php
    typo fix

--- formlar/gunlukbakimuygulamalari.php
+++ formlar/gunlukbakimuygulamalari.php
@@ -60,7 +60,7 @@ if (isset($_GET['logout'])) {
                 <div class="patients-save">
                     <form action="" method="POST" class="patients-save-fields">
                     <label for="uygulamalar">Uygulama Seçiniz:</label>
-                    <<select id="uygulamalar" name="uygulamalar">
+                    <select id="uygulamalar" name="uygulamalar">
                         <option value="Ağız bakımı">Ağız bakımı</option>
                         <option value="Yüz bakımı">Yüz bakımı</option>
                         <option value="El Bakımı">El Bakımı</option>
