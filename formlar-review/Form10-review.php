9564c2be8bf400da32811d4f29fd7492a0332ba4
AtaTasyapar <mert.tasyapar@ug.bilkent.edu.tr>
2023-05-15 14:11:23 +0300

    tanı1 test

--- formlar-review/Form10-review.php
+++ formlar-review/Form10-review.php
@@ -385,20 +385,19 @@ if (isset($_GET['logout'])) {
     var respiratory_rate = parseInt($("input[name='respiratory_rate']").val());
     console.log("AAAAAA");
     console.log(respiratory_rate);
-    console.log(<?php $respiratory_rate ?>);
-    // if (<?php $respiratory_rate ?> < 16) {
-    //     $('#tanı1-warning').css("display", "block");
-    //     $(function() {
-    //         $("a.addtanı").on("click", function(e) {
-    //             e.preventDefault();
-    //             console.log("tanı111111");
-    //             // var url = "<?php echo $base_url; ?>/updateForms/showAllForms.php";
-    //             // $("#content").load(url);
-    //         });
-    //     });
-    // } else {
-    //     $('#tanı1-warning').css("display", "none");
-    // }
+    if (respiratory_rate < 16 || respiratory_rate > 20) {
+        $('#tanı1-warning').css("display", "block");
+        $(function() {
+            $("a.addtanı").on("click", function(e) {
+                e.preventDefault();
+                console.log("tanı111111");
+                // var url = "<?php echo $base_url; ?>/updateForms/showAllForms.php";
+                // $("#content").load(url);
+            });
+        });
+    } else {
+        $('#tanı1-warning').css("display", "none");
+    }
     </script>
     <script>
     $(function() {
