0eea586692bb23167a3f10fad59f86c3d7e6759f
ozanergun98 <99257197+ozanergun98@users.noreply.github.com>
2023-05-18 00:16:49 +0300

    Update bosaltimForm1.php

--- formlar/bosaltimForm1.php
+++ formlar/bosaltimForm1.php
@@ -615,6 +615,7 @@ if (isset($_GET['logout'])) {
                         </div>
                     </div>
                 </div>
+                                    <input type="submit" class="form-control submit" name="submit" id="submit" value="Kaydet">
             </div>
 
             <script>
