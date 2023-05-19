f610fdb264d8cf66cfc921fcd3715765c93ded43
Semih Demirler <semihdemirler@gmail.com>
2023-05-19 06:02:15 +0300

    Update tetkiksonuclari_form9.php
    fix

--- formlar/tetkiksonuclari_form9.php
+++ formlar/tetkiksonuclari_form9.php
@@ -126,11 +126,11 @@ if (isset($_GET['logout'])) {
                         </div>
                         <div class="input-section d-flex">
                             <p class="usernamelabel">Tetkik Sonucu :</p>
-                            <input type="text" class="form-control" required name="examination_result" id="diger" placeholder="Tetkik Sonucu" maxlenght = "2000">
+                            <input type="text" class="form-control" required name="examination_result" id="diger" placeholder="Tetkik Sonucu" maxlength = "2000">
                         </div>
                         <div class="input-section d-flex">
                             <p class="usernamelabel">Referans Değeri :</p>
-                            <input type="text" class="form-control" required name="referance_value" id="diger" placeholder="Referans Değeri" maxlenght="5">
+                            <input type="text" class="form-control" required name="referance_value" id="diger" placeholder="Referans Değeri" maxlength="5">
                         </div>
                         <input class="form-control submit" type="submit" name="submit" id="submit" value="Submit">
                     </form>
