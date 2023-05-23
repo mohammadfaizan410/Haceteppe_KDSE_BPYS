0e71e8cdc571154dc336a4c76478bc7756859837
Semih Demirler <semihdemirler@gmail.com>
2023-05-23 11:51:13 +0300

    new conf

--- config-messages.php
+++ config-messages.php
@@ -1,6 +1,6 @@
 <?php
 $db_user = "admin";
-$db_pass = "12345678";
+$db_pass = "CTISsenior123";
 $db_name = "kdse_db";
 $db_host = "ebyrys.cpcbxswdfvjg.eu-central-1.rds.amazonaws.com";
 $db = new PDO('mysql:host=' . $db_host .';dbname=' . $db_name . ';charset-utf8', $db_user, $db_pass);
