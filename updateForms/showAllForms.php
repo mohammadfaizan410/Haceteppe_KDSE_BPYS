f9b332572fa124cd77787e5ef807100630530022
ozanergun98 <99257197+ozanergun98@users.noreply.github.com>
2023-06-10 00:49:03 +0300

    Update showAllForms.php

--- updateForms/showAllForms.php
+++ updateForms/showAllForms.php
@@ -301,8 +301,39 @@ if (isset($_GET['logout'])) {
                 <div class="table-responsive">
                     <h1 class='mb-5 entered-forms-header'>Yeni Form Doldur</h1>
                     <div class="entered-forms-wrapper">
-                         <div class="mt-3 entered-forms"><a class="nav-items newForm" style="color: white;"
-                                href="<?php echo $base_url; ?>/updateForms/showAllForms.php?patient_id=<?php echo $userid; ?>&patient_name=<?php echo $patient_name; ?>">Form 1</a></div>
+                                                <div class="mt-3 entered-forms"><a class="nav-items newForm" style="color : white;"
+                               href="<?php echo $base_url; ?>/formlar/beslenmeGereksinimi_form1.php?patient_id=<?php echo $userid; ?>&patient_name=<?php echo $patient_name; ?>">Form1_beslenme</a>
+                        </div>
+                        <div class="mt-3 entered-forms"><a class="nav-items newForm" style="color : white;"
+                                href="<?php echo $base_url; ?>/formlar/bosaltimForm1.php?patient_id=<?php echo $userid; ?>&patient_name=<?php echo $patient_name; ?>">bosaltimForm1</a>
+                        </div>
+                        <div class="mt-3 entered-forms"><a class="nav-items newForm" style="color : white;"
+                                href="<?php echo $base_url; ?>/formlar/calismaForm1.php?patient_id=<?php echo $userid; ?>&patient_name=<?php echo $patient_name; ?>">calismaForm1</a>
+                        </div>
+                        <div class="mt-3 entered-forms"><a class="nav-items newForm" style="color : white;"
+                                href="<?php echo $base_url; ?>/formlar/egitimForm1.php?patient_id=<?php echo $userid; ?>&patient_name=<?php echo $patient_name; ?>">egitimForm1</a>
+                        </div>
+                        <div class="mt-3 entered-forms"><a class="nav-items newForm" style="color : white;"
+                                href="<?php echo $base_url; ?>/formlar/hareketForm1.php?patient_id=<?php echo $userid; ?>&patient_name=<?php echo $patient_name; ?>">hareketForm1</a>
+                        </div>
+                        <div class="mt-3 entered-forms"><a class="nav-items newForm" style="color : white;"
+                                href="<?php echo $base_url; ?>/formlar/iletisimForm1.php?patient_id=<?php echo $userid; ?>&patient_name=<?php echo $patient_name; ?>">iletisimForm1</a>
+                        </div>
+                        <div class="mt-3 entered-forms"><a class="nav-items newForm" style="color : white;"
+                                href="<?php echo $base_url; ?>/formlar/kateterForm1.php?patient_id=<?php echo $userid; ?>&patient_name=<?php echo $patient_name; ?>">kateterForm1</a>
+                        </div>
+                        <div class="mt-3 entered-forms"><a class="nav-items newForm" style="color : white;"
+                                href="<?php echo $base_url; ?>/formlar/ozgecmis_form1.php?patient_id=<?php echo $userid; ?>&patient_name=<?php echo $patient_name; ?>">ozgecmis_form1</a>
+                        </div>
+                        <div class="mt-3 entered-forms"><a class="nav-items newForm" style="color : white;"
+                                href="<?php echo $base_url; ?>/formlar/solunumgereksinimi_form1.php?patient_id=<?php echo $userid; ?>&patient_name=<?php echo $patient_name; ?>">solunumgereksinimi_form1</a>
+                        </div>
+                        <div class="mt-3 entered-forms"><a class="nav-items newForm" style="color : white;"
+                                href="<?php echo $base_url; ?>/formlar/uykuForm1.php?patient_id=<?php echo $userid; ?>&patient_name=<?php echo $patient_name; ?>">uykuForm1</a>
+                        </div>
+                        <div class="mt-3 entered-forms"><a class="nav-items newForm" style="color : white;"
+                                href="<?php echo $base_url; ?>/formlar/vucuduTemizForm1.php?patient_id=<?php echo $userid; ?>&patient_name=<?php echo $patient_name; ?>">vucuduTemizForm1</a>
+                        </div>
                         <div class="mt-3 entered-forms"><a class="nav-items newForm" style="color: white;"
                                 href="<?php echo $base_url; ?>/formlar/Form2.php?patient_id=<?php echo $userid; ?>&patient_name=<?php echo $patient_name; ?>">Form
                                 2</a></div>
