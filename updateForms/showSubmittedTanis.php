b8e21cbde619884ec5af1147ac672230ef5fd18b
Muhammad Faizan <40329896+mohammadfaizan410@users.noreply.github.com>
2023-07-02 03:14:31 +0300

    root_id/parent_id get

--- updateForms/showSubmittedTanis.php
+++ updateForms/showSubmittedTanis.php
@@ -45,7 +45,7 @@ if (isset($_GET['logout'])) {
         require_once('../config-students.php');
         $userid = $_SESSION['userlogin']['id'];
         $patientId = $_GET['patient_id'];
-        $sql = "SELECT * FROM tani WHERE patient_id = " . $patientId . " AND standalone = 'true' GROUP BY tani_num ORDER BY creation_date DESC";
+        $sql = "SELECT * FROM tani WHERE patient_id = " . $patientId . "";
         $smtmselect = $db->prepare($sql);
         $result = $smtmselect->execute();
         if ($result) {
