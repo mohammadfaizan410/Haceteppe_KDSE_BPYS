5d40b1ef826f9b036d883c59c07f9b8b68c6747f
Muhammad Faizan <40329896+mohammadfaizan410@users.noreply.github.com>
2023-07-04 04:21:04 +0300

    button fix

--- taniReview/tani4-review.php
+++ taniReview/tani4-review.php
@@ -529,13 +529,7 @@ if ($result) {
                             </div>
                            
                         </div>
-                        <?php 
-                            if ($_GET['display'] !== 0) {
-                                echo '<input type="submit" class="d-flex w-75 submit m-auto justify-content-center mb-5" style="display: block" name="submit" id="submit" value="Kaydet">';              
-                            }
-
-                        ?>    
-
+                        
 
 
                     </form>
@@ -563,6 +557,9 @@ if ($result) {
 
             })
         });
+        if(<?php echo $_GET['display']; ?> === 1){
+        $('form').append('<input type="submit" class="d-flex w-75 submit m-auto justify-content-center mb-5" style="display: block" name="submit" id="submit" value="Kaydet">');
+        }
         $('input[name="noc_indicator"][value="<?php echo $tani4[0]['noc_indicator']; ?>"]').prop('checked', true);
         $('input[name="noc_indicator_2"][value="<?php echo $tani4[0]['noc_indicator_2']; ?>"]').prop('checked', true);
         $('input[name="noc_indicator_3"][value="<?php echo $tani4[0]['noc_indicator_3']; ?>"]').prop('checked', true);
